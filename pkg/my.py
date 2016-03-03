28d6cb04e10a6e8652dd9daf60ab2567781f9e38
Matthias Lehmann <matthias.lehmann@qsensei.com>
2016-03-03 17:30:49 +0100

    feature: some improvements to schema
    - use Fortune 500 for keyword extraction
    - index mime-type

--- pkg/my.py
+++ pkg/my.py
@@ -1,8 +1,9 @@
 import os
 
+
 def location(terms):
-  for term in terms:
-    dirname = os.path.dirname(term)
-    for segment in dirname.split(os.path.sep):
-	if segment:
-           yield segment
+    for term in terms:
+        dirname = os.path.dirname(term)
+        for segment in dirname.split(os.path.sep):
+            if segment:
+                yield segment
