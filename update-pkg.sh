f5c14d4ae7f29c2d99941f3187b90bd5717a99ce
Matthias Lehmann <matthias.lehmann@qsensei.com>
2016-03-21 17:15:43 +0100

    fix: use install insteas of setup in update-pkg.sh

--- update-pkg.sh
+++ update-pkg.sh
@@ -3,4 +3,4 @@
 # if docker-compose was installed via buildout it is in $PWD/bin
 PATH=$PWD/bin:$PATH
 . ./env.sh
-docker-compose run --rm fuse fusectl setup /fuse/ext/pkg
+docker-compose run --rm fuse fusectl install -f -p /fuse/ext/pkg
